e3df6988448bc70ffdf00e2ef90d817e11611595
Sogdian Ashurov <sogdian4815@gmail.com>
2025-06-02 22:45:48 +0200

    add devtool

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
+  devtool: 'eval-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
