874765e35096e7ba7cd7e1bc53459a366168a425
Sogdian Ashurov <sogdian4815@gmail.com>
2025-06-02 23:18:00 +0200

    поправил минимизацию

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
-  devtool: 'eval-source-map',
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
