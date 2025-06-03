df259a1042a08996458d63ecdb418fa94f574833
Sogdian Ashurov <sogdian4815@gmail.com>
2025-06-03 14:48:42 +0200

    поправил doctor

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     clean: true,
-    publicPath: '/',
+    publicPath: '/doctor/',
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
