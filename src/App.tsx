2a5c55185ce332e0b00f65f14cb0daa75c1e77f2
Sogdian Ashurov <sogdian4815@gmail.com>
2025-06-03 14:38:29 +0200

    добавил модель, работает

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
-import {Header} from './components/Header/Header';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
 
-const App: React.FC = () => {
-  return (
-    <div>
-      <Header />
-      <main>
-        <h2>Добро пожаловать в новый React + TypeScript проект!</h2>
-      </main>
+function Model() {
+  // Путь относительно src!
+  const { scene } = useGLTF('/models/shoes_test_1-v1.glb');
+  return <primitive object={scene} scale={2} />;
+}
+
+const App: React.FC = () => (
+  <div>
+    <h1>Детский доктор Анна Иванова</h1>
+    <div style={{ width: 400, height: 400, margin: '0 auto' }}>
+      <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+        <ambientLight intensity={0.7} />
+        <directionalLight position={[5, 5, 5]} intensity={0.7} />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
     </div>
-  );
-};
+  </div>
+);
 
 export default App; 
