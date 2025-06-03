57d5c550812177db7731d757e5fcde728200bf7a
Sogdian Ashurov <sogdian4815@gmail.com>
2025-06-03 20:32:36 +0200

    добавил absolute position

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -29,6 +29,20 @@ const DoctorModel = () => {
 
 export const LandingPage: React.FC = () => (
   <div className="landing">
+    <div
+      className="background-3d"
+      style={{ width: 300, height: 300, margin: '24px auto 0 auto' }}
+    >
+      <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+        <ambientLight intensity={0.7} />
+        <directionalLight position={[5, 5, 5]} intensity={0.7} />
+        <Suspense fallback={null}>
+          <DoctorModel />
+        </Suspense>
+        <OrbitControls enablePan={false} />
+      </Canvas>
+    </div>
+
     <nav className="navbar">
       <div className="logo">Doctor<span className="plus">+</span></div>
       <ul className="menu">
@@ -45,16 +59,7 @@ export const LandingPage: React.FC = () => (
         <h1 className="title">A dedicated doctor<br/>you can trust</h1>
         <p className="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum eget vel, nunc nulla feugiat. Metus ut.</p>
         <button className="cta">Book an appointment</button>
-        <div style={{ width: 300, height: 300, margin: '24px auto 0 auto' }}>
-          <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
-            <ambientLight intensity={0.7} />
-            <directionalLight position={[5, 5, 5]} intensity={0.7} />
-            <Suspense fallback={null}>
-              <DoctorModel />
-            </Suspense>
-            <OrbitControls enablePan={false} />
-          </Canvas>
-        </div>
+        
       </section>
       <section className="illustration">
         <img src={doctorImg} alt="Doctor" className="doctorImg" />
