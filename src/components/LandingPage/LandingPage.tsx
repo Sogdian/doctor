dfc563069c380decb332b402c4bfc433b3a39549
Sogdian Ashurov <sogdian4815@gmail.com>
2025-06-05 21:24:31 +0200

    сделал модель

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -24,14 +24,12 @@ const Canva = () => {
   return (
     <div
       className="background-3d"
-      style={{ width: 300, height: 300, margin: '24px auto 0 auto' }}
     >
-      <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
-        <directionalLight position={[5, 5, 5]} intensity={0.7} />
+      <Canvas camera={{ position: [5, 9, 10], fov: 45 }}>
+        <directionalLight position={[0, 10, 0]} intensity={0.8} />
         <Suspense fallback={null}>
           <DoctorModel />
         </Suspense>
-        {/* OrbitControls убран */}
       </Canvas>
     </div>
   );
