6dd4a466b41c091bc7f2d9cb863c6cf781054af4
Sogdian Ashurov <sogdian4815@gmail.com>
2025-06-05 21:01:31 +0200

    должны проигрываться анимации

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,29 +1,42 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import React, { Suspense, useEffect, useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Group } from 'three';
+import { OrbitControls, useAnimations, useGLTF } from '@react-three/drei';
 import './LandingPage.module.less';
 
 const DoctorModel = () => {
-  const { scene } = useGLTF('models/man.glb');
-  return <primitive object={scene} scale={2} />;
+  const group = useRef<Group>(null);
+  const { scene, animations } = useGLTF('models/Box.glb');
+  const { actions } = useAnimations(animations, group);
+
+  useEffect(() => {
+    const actionNames = Object.keys(actions || {});
+    if (actionNames.length > 0) {
+      const firstAction = actions?.[actionNames[0]];
+      firstAction?.reset().play();
+    }
+  }, [actions]);
+
+  return <primitive ref={group} object={scene} scale={2} />;
 };
 
 const Canva = () => {
   return (
-  <div
-          className="background-3d"
-          style={{ width: 300, height: 300, margin: '24px auto 0 auto' }}
-          >
+    <div
+      className="background-3d"
+      style={{ width: 300, height: 300, margin: '24px auto 0 auto' }}
+    >
       <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
         <directionalLight position={[5, 5, 5]} intensity={0.7} />
         <Suspense fallback={null}>
           <DoctorModel />
         </Suspense>
-        <OrbitControls enablePan={false} />
+        {/* OrbitControls убран */}
       </Canvas>
     </div>
-  )
+  );
 };
+
 
 import mainImg from '../../assets/main-kid.png';
 import card1 from '../../assets/card1.png';
