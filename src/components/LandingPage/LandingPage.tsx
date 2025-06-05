ebbe34ecea444115cbc2af3668c4ca5a3150f9b0
Sogdian Ashurov <sogdian4815@gmail.com>
2025-06-05 09:52:09 +0200

    delete ambientLight

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -66,7 +66,6 @@ export const LandingPage: React.FC = () => (
       style={{ width: 300, height: 300, margin: '24px auto 0 auto' }}
     >
       <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
-        <ambientLight intensity={0.7} />
         <directionalLight position={[5, 5, 5]} intensity={0.7} />
         <Suspense fallback={null}>
           <DoctorModel />
